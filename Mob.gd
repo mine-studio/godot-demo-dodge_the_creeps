e28c734037c681a54ede4689bc6ef714180e0830
Micfan <micfan@users.noreply.github.com>
2019-05-23 00:59:39 +0800

    make Enemy animation start()

--- Mob.gd
+++ Mob.gd
@@ -7,10 +7,12 @@ var mob_types = ["walk", "swim", "fly"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$AnimatedSprite.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_Visibility_screen_exited():
-    queue_free()
+	queue_free()
+	$AnimatedSprite.stop()
